2be34b49ca241744623149dd23810dac978ba2ca
Thomas Hornschuh <thomas@hornschuh-family.de>
2019-12-20 18:03:26 +0100

    Optional support for RVTEST_IO_ASSERT_GPR_EQ macro to debug test benches

--- riscv-target/bonfire/compliance_io.h
+++ riscv-target/bonfire/compliance_io.h
@@ -15,7 +15,20 @@
 #define RVTEST_IO_INIT
 #define RVTEST_IO_PUTC(_R)
 #define RVTEST_IO_WRITE_STR(_SP, _STR)
-#define RVTEST_IO_ASSERT_GPR_EQ(_SP, _R, _I)
+
+
+// _SP = (volatile register)
+// _R = GPR
+// _I = Immediate
+#define RVTEST_IO_ASSERT_GPR_EQ(_SP, _R, _I) \
+//    li _SP, _I; \
+//    beq _SP,_R, 20002f; \
+//    mv x31,_R; \
+//    ecall; \
+//    la _SP, monitor; \
+//    sw _R, 0(_SP); \
+// 20002:          \
+
 #define RVTEST_IO_CHECK()
 
 #endif // _COMPLIANCE_IO_H
