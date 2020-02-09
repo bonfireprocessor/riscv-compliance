1363057113650c22e4a9f23c5e820475ecb032f2
Thomas Hornschuh <thomas@hornschuh-family.de>
2020-02-09 17:15:50 +0100

    Added bonfire-core to targets

---
+++ riscv-target/bonfire-core/compliance_test.h
@@ -0,0 +1,67 @@
+// RISC-V Compliance Test Header File
+// Copyright (c) 2017, Codasip Ltd. All Rights Reserved.
+// See LICENSE for license details.
+//
+// Description: Common header file for RV32I tests
+
+#ifndef _COMPLIANCE_TEST_H
+#define _COMPLIANCE_TEST_H
+
+#include "riscv_test.h"
+
+//-----------------------------------------------------------------------
+// RV Compliance Macros
+//-----------------------------------------------------------------------
+
+#define TESTUTIL_BASE monitor
+#define TESTUTIL_ADDR_HALT (TESTUTIL_BASE)
+#define TESTUTIL_ADDR_BEGIN_SIGNATURE (TESTUTIL_BASE + 0x8)
+#define TESTUTIL_ADDR_END_SIGNATURE (TESTUTIL_BASE + 0xc)
+
+#undef RVTEST_PASS
+#define RVTEST_PASS \
+        li t0, 1;                                                             \
+        la t1, TESTUTIL_ADDR_HALT;                                            \
+        sw t0, 0(t1);    \
+        _endless: \
+        j _endless; \
+
+#undef RVTEST_FAIL
+#define RVTEST_FAIL \
+        li t0, -1;                                                             \
+        la t1, TESTUTIL_ADDR_HALT;                                            \
+        sw t0, 0(t1);    \
+        j _endless; \
+
+
+#define RV_COMPLIANCE_HALT                                                    \
+        RVTEST_PASS                                                           \
+
+
+#define RV_COMPLIANCE_RV32M   \
+        .section .text.init; \
+        .global _start;       \
+        _start:              \
+        li sp,0x7ff0;        \
+        j begin_testcode; \
+
+
+#define RV_COMPLIANCE_CODE_BEGIN                                              \
+        .section .text;                                                  \
+        .align  4;                                                            \
+        .global begin_testcode;                                                        \
+         begin_testcode:
+
+
+
+#define RV_COMPLIANCE_CODE_END                                                \
+                                                                              \
+
+#define RV_COMPLIANCE_DATA_BEGIN \
+        RVTEST_DATA_BEGIN \
+
+#define RV_COMPLIANCE_DATA_END           \
+         RVTEST_DATA_END    \
+
+#endif
+
